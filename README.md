eb686f3d7c0aa1ee65a09c284e9506e6d5328872
marttsang2 <tsangmartin1225@gmail.com>
2021-01-19 15:30:31 +0800

    no message

--- README.md
+++
@@ -1,3 +0,0 @@
-# Trasy Website
-
-This is a Trasy-app Website introducing the free trading mobile application.
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import MinimalPortfolio from "@openarchitex/gatsby-theme-minimal-portfolio/src/templates/minimal-portfolio";
-
-export default function Home() {
-  return <MinimalPortfolio />
-}
